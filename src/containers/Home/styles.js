48cf5d8773382fc4eb49eeaae5cbb942d2345eb2
rogeriobarreto27 <rogeriooo.silva@gmail.com>
2024-03-22 14:01:09 -0300

    first commit

---
+++ src/containers/Home/styles.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import Backgroud from "../../assets/background.svg"
+
+export const Container = styled.div`
+    background: url("${Backgroud}");
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+    height: 100vh;
+`;
+
+export const Image = styled.img`
+    margin-top: 30px;
+`;
+
+export const ContainerItens = styled.div`
+    background: linear-gradient(
+        157.44deg, 
+        rgba(255, 255, 255, 0.6) 0.84%, 
+        rgba(255, 255, 255, 0.6) 0.85%, 
+        rgba(255, 255, 255, 0.15) 100%
+        );
+    border-radius: 61px 61px 0px 0px;
+    padding: 50px 36px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+
+`;
+
+export const H1 = styled.h1`
+font-size: 34px;
+font-weight: 700;
+line-height: 40px;
+text-align: center;
+color: #FFFFFF;
+margin-bottom: 80px;
+
+`;
+
+export const InputLabel = styled.p`
+font-size: 18px;
+font-weight: 700;
+line-height: 22px;
+letter-spacing: -0.40799999237060547px;
+text-align: left;
+color: #FFFFFF;
+padding-left: 25px;
+
+`;
+
+export const Input = styled.input`
+   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+   background: rgba(255, 255, 255, 0.25);
+   border-radius: 14px;
+   width: 342px;
+   height: 58px;
+   padding-left: 25px;
+   border: none;
+   outline: none;
+   color: #FFFFFF;
+   font-size: 24px;
+   line-height: 28px;
+   margin-bottom: 35px;
+
+`;
+
+export const Button = styled.button`
+    width: 342px;
+    height: 74px;
+    margin-top: 130px;
+    border-radius: 14px;
+    border: none;
+    background: rgba(0, 0, 0, 0.8);
+    color: #FFFFFF;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 17px;
+    line-height: 28px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+
+    &:hover{opacity: 0.8}
+    &:active{opacity: 0.5}
+
+`;
